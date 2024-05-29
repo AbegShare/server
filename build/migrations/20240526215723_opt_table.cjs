88f9fb2535c448ebee838ae93d64ae4811f7e2a2
wami ikechukwu <wamiikechukwu@gmail.com>
2024-05-29 22:30:34 +0100

    refactor: renamed the otp table and column

--- build/migrations/20240526215723_opt_table.cjs
+++ build/migrations/20240526215723_opt_table.cjs
@@ -4,15 +4,15 @@
  */
 
 exports.up = async function (knex) {
-    return await knex.schema.createTable('OTP', function (table) {
+    return await knex.schema.createTable('otp', function (table) {
         table.specificType('id', 'char(36) primary key');
-        table.string('OTP', 5).notNullable(); // OTP as a string of 5 characters
+        table.string('otp_code', 5).notNullable(); // OTP as a string of 5 characters
         table.timestamp('created_at').notNullable().defaultTo(knex.fn.now()); // created_at with the current timestamp
         table.timestamp('expires_at').notNullable() // expires_at with current timestamp + 24 hours
         table.string('user_id', 36).notNullable().references('id').inTable('user').onDelete('cascade')
     }).then(() => knex.raw(`
       CREATE TRIGGER set_expires_at
-      BEFORE INSERT ON OTP
+      BEFORE INSERT ON otp
       FOR EACH ROW
       SET NEW.expires_at = DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 24 HOUR);
     `))
